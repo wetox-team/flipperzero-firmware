c72802ea10d92a92ede7dc863cefaea3c5867537
Alex Grishin <alexgr13@gmail.com>
2021-12-04 18:58:50 +0300

    New app

--- applications/applications.c
+++ applications/applications.c
@@ -43,6 +43,7 @@ extern int32_t vibro_test_app(void* p);
 // Plugins
 extern int32_t music_player_app(void* p);
 extern int32_t snake_game_app(void* p);
+extern int32_t tanks_game_app(void* p);
 
 // On system start hooks declaration
 extern void bt_cli_init();
@@ -211,6 +212,10 @@ const FlipperApplication FLIPPER_PLUGINS[] = {
 
 #ifdef APP_SNAKE_GAME
     {.app = snake_game_app, .name = "Snake Game", .stack_size = 1024, .icon = &A_Plugins_14},
+#endif
+
+#ifdef APP_TANKS_GAME
+    {.app = tanks_game_app, .name = "Tanks Game", .stack_size = 1024, .icon = &A_Plugins_14},
 #endif
 };
 
