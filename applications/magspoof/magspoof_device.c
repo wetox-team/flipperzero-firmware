ff6e33ba25f0bf91b4baf416e572157b5a08c748
Sasha Yaroshevich <sasha.yaroshevich@devunit.info>
2021-12-05 13:28:38 +0300

    added magspoof device

---
+++ applications/magspoof/magspoof_device.c
@@ -0,0 +1,239 @@
+#include "magspoof_device.h"
+
+#include <lib/toolbox/path.h>
+#include <lib/flipper_file/flipper_file.h>
+
+static const char* magspoof_app_folder = "/any/magspoof";
+static const char* magspoof_app_extension = ".magspoof";
+static const char* magspoof_app_shadow_extension = ".shd";
+static const char* magspoof_file_header = "Flipper Magspoof device";
+static const uint32_t magspoof_file_version = 1;
+
+MagspoofDevice* magspoof_device_alloc() {
+    MagspoofDevice* magspoof_dev = furi_alloc(sizeof(MagspoofDevice));
+    magspoof_dev->storage = furi_record_open("storage");
+    magspoof_dev->dialogs = furi_record_open("dialogs");
+    return magspoof_dev;
+}
+
+void magspoof_device_free(MagspoofDevice* magspoof_dev) {
+    furi_assert(magspoof_dev);
+    furi_record_close("storage");
+    furi_record_close("dialogs");
+    free(magspoof_dev);
+}
+
+void nfc_device_prepare_format_string(NfcDevice* dev, string_t format_string) {
+    if(dev->format == NfcDeviceSaveFormatUid) {
+        string_set_str(format_string, "UID");
+    } else if(dev->format == NfcDeviceSaveFormatBankCard) {
+        string_set_str(format_string, "Bank card");
+    } else if(dev->format == NfcDeviceSaveFormatMifareUl) {
+        string_set_str(format_string, "Mifare Ultralight");
+    } else {
+        string_set_str(format_string, "Unknown");
+    }
+}
+
+bool nfc_device_parse_format_string(NfcDevice* dev, string_t format_string) {
+    if(string_start_with_str_p(format_string, "UID")) {
+        dev->format = NfcDeviceSaveFormatUid;
+        dev->dev_data.nfc_data.protocol = NfcDeviceProtocolUnknown;
+        return true;
+    } else if(string_start_with_str_p(format_string, "Bank card")) {
+        dev->format = NfcDeviceSaveFormatBankCard;
+        dev->dev_data.nfc_data.protocol = NfcDeviceProtocolEMV;
+        return true;
+    } else if(string_start_with_str_p(format_string, "Mifare Ultralight")) {
+        dev->format = NfcDeviceSaveFormatMifareUl;
+        dev->dev_data.nfc_data.protocol = NfcDeviceProtocolMifareUl;
+        return true;
+    }
+    return false;
+}
+
+static bool magspoof_device_save_data(FlipperFile* file, MagspoofDevice* dev) {
+    bool saved = false;
+    MagspoofDeviceCommonData* data = &dev->dev_data.magspoof_data;
+    uint32_t data_temp = 0;
+
+    do {
+        if(!flipper_file_write_string(file, "Data", data->data)) break;
+        saved = true;
+    } while(false);
+
+    return saved;
+}
+
+bool magspoof_device_load_common_data(FlipperFile* file, NfcDevice* dev) {
+    bool parsed = false;
+    MagspoofDeviceCommonData* data = &dev->dev_data.magspoof_data;
+    memset(data, 0, sizeof(MagspoofDeviceCommonData));
+    uint32_t data_cnt = 0;
+    string_t temp_str;
+    string_init(temp_str);
+
+    do {
+        if(!flipper_file_read_string(file, "Data", temp_str)) break;
+        strlcpy(data->data, string_get(temp_str), sizeof(data->data));
+    } while(false);
+
+    string_clear(temp_str);
+    return parsed;
+}
+
+void magspoof_device_set_name(MagspoofDevice* dev, const char* name) {
+    furi_assert(dev);
+
+    strlcpy(dev->dev_name, name, MAGSPOOF_DEV_NAME_MAX_LEN);
+}
+
+static bool nfc_device_save_file(
+    MagspoofDevice* dev,
+    const char* dev_name,
+    const char* folder,
+    const char* extension) {
+    furi_assert(dev);
+
+    bool saved = false;
+    FlipperFile* file = flipper_file_alloc(dev->storage);
+    MagspoofDeviceCommonData* data = &dev->dev_data.magspoof_data;
+    string_t temp_str;
+    string_init(temp_str);
+
+    do {
+        // Create magspoof directory if necessary
+        if(!storage_simply_mkdir(dev->storage, magspoof_app_folder)) break;
+        // First remove magspoof device file if it was saved
+        string_printf(temp_str, "%s/%s%s", folder, dev_name, extension);
+        // Open file
+        if(!flipper_file_open_always(file, string_get_cstr(temp_str))) break;
+        // Write header
+        if(!flipper_file_write_header_cstr(file, nfc_file_header, nfc_file_version)) break;
+        
+        if(!nfc_device_save_data(file, dev)) break;
+        
+        saved = true;
+    } while(0);
+
+    if(!saved) {
+        dialog_message_show_storage_error(dev->dialogs, "Can not save\nkey file");
+    }
+    string_clear(temp_str);
+    flipper_file_close(file);
+    flipper_file_free(file);
+    return saved;
+}
+
+bool magspoof_device_save(MagspoofDevice* dev, const char* dev_name) {
+    return nfc_device_save_file(dev, dev_name, nfc_app_folder, nfc_app_extension);
+}
+
+static bool maspoof_device_load_data(MagspoofDevice* dev, string_t path) {
+    bool parsed = false;
+    FlipperFile* file = flipper_file_alloc(dev->storage);
+    MagspoofDeviceCommonData* data = &dev->dev_data.magspoof_data;
+    uint32_t data_cnt = 0;
+    string_t temp_str;
+    string_init(temp_str);
+    bool depricated_version = false;
+
+    do {
+        if(!flipper_file_open_existing(file, string_get_cstr(path))) break;
+        // Read and verify file header
+        uint32_t version = 0;
+        if(!flipper_file_read_header(file, temp_str, &version)) break;
+        if(string_cmp_str(temp_str, nfc_file_header) || (version != nfc_file_version)) {
+            depricated_version = true;
+            break;
+        }
+    
+        if(!nfc_device_load_common_data(file, dev)) break;
+
+        parsed = true;
+    } while(false);
+
+    if(!parsed) {
+        if(depricated_version) {
+            dialog_message_show_storage_error(dev->dialogs, "File format depricated");
+        } else {
+            dialog_message_show_storage_error(dev->dialogs, "Can not parse\nfile");
+        }
+    }
+
+    string_clear(temp_str);
+    flipper_file_close(file);
+    flipper_file_free(file);
+    return parsed;
+}
+
+bool magspoof_device_load(MagspoofDevice* dev, const char* file_path) {
+    furi_assert(dev);
+    furi_assert(file_path);
+
+    // Load device data
+    string_t path;
+    string_init_set_str(path, file_path);
+    bool dev_load = magspoof_device_load_data(dev, path);
+    if(dev_load) {
+        // Set device name
+        path_extract_filename_no_ext(file_path, path);
+        magspoof_device_set_name(dev, string_get_cstr(path));
+    }
+    string_clear(path);
+
+    return dev_load;
+}
+
+bool magspoof_file_select(MagspoofDevice* dev) {
+    furi_assert(dev);
+
+    // Input events and views are managed by file_select
+    bool res = dialog_file_select_show(
+        dev->dialogs,
+        magspoof_app_folder,
+        magspoof_app_extension,
+        dev->file_name,
+        sizeof(dev->file_name),
+        dev->dev_name);
+    if(res) {
+        string_t dev_str;
+        // Get key file path
+        string_init_printf(dev_str, "%s/%s%s", magspoof_app_folder, dev->file_name, magspoof_app_extension);
+        res = magspoof_device_load_data(dev, dev_str);
+        if(res) {
+            magspoof_device_set_name(dev, dev->file_name);
+        }
+        string_clear(dev_str);
+    }
+
+    return res;
+}
+
+void magspoof_device_clear(MagspoofDevice* dev) {
+    furi_assert(dev);
+
+    memset(&dev->dev_data, 0, sizeof(dev->dev_data));
+}
+
+bool magspoof_device_delete(MagspoofDevice* dev) {
+    furi_assert(dev);
+
+    bool deleted = false;
+    string_t file_path;
+    string_init(file_path);
+
+    do {
+        // Delete original file
+        string_init_printf(file_path, "%s/%s%s", magspoof_app_folder, dev->dev_name, magspoof_app_extension);
+        if(!storage_simply_remove(dev->storage, string_get_cstr(file_path))) break;
+        deleted = true;
+    } while(0);
+
+    if(!deleted) {
+        dialog_message_show_storage_error(dev->dialogs, "Can not remove file");
+    }
+
+    string_clear(file_path);
+    return deleted;
+}
---
+++ applications/magspoof/magspoof_device.h
@@ -0,0 +1,48 @@
+#pragma once
+
+#include <stdint.h>
+#include <stdbool.h>
+#include <m-string.h>
+#include <storage/storage.h>
+#include <dialogs/dialogs.h>
+
+#include "mifare_ultralight.h"
+
+
+#define MAGSPOOF_DEV_NAME_MAX_LEN 22
+#define MAGSPOOF_FILE_NAME_MAX_LEN 120
+
+
+
+typedef struct {
+    string_t data[21*6];
+} MagspoofDeviceCommonData;
+
+typedef struct {
+    MagspoofDeviceCommonData magspoof_data;
+} MagspoofDeviceData;
+
+typedef struct {
+    Storage* storage;
+    DialogsApp* dialogs;
+    MagspoofDeviceData dev_data;
+    char dev_name[MAGSPOOF_DEV_NAME_MAX_LEN + 1];
+    char file_name[MAGSPOOF_FILE_NAME_MAX_LEN];
+    bool shadow_file_exist;
+} MagspoofDevice;
+
+MagspoofDevice* magspoof_device_alloc();
+
+void magspoof_device_free(MagspoofDevice* nfc_dev);
+
+void magspoof_device_free_device_set_name(MagspoofDevice* dev, const char* name);
+
+bool magspoof_device_save(MagspoofDevice* dev, const char* dev_name);
+
+bool magspoof_device_load(MagspoofDevice* dev, const char* file_path);
+
+bool magspoof_file_select(MagspoofDevice* dev);
+
+void magspoof_device_clear(MagspoofDevice* dev);
+
+bool magspoof_device_delete(MagspoofDevice* dev);
