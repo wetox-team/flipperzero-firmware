a8a190b242c2d143bd3aea9b03b0f1dc26c9d895
Astra <astra@astrra.space>
2022-07-01 14:38:28 +0300

    I now know how scons works, apparently

--- applications/desktop/desktop_settings/scenes/desktop_settings_scene_start.c
+++ applications/desktop/desktop_settings/scenes/desktop_settings_scene_start.c
@@ -87,6 +87,7 @@ void desktop_settings_scene_start_on_enter(void* context) {
         app);
 
     value_index = value_index_uint32(app->settings.is_inverted, inverted_value, INVERTED_COUNT);
+    variable_item_set_current_value_index(item, value_index);
     variable_item_set_current_value_text(item, inverted_text[value_index]);
 
     variable_item_list_set_enter_callback(
